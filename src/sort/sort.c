c250185ea3ed5b3357b970be487bf9971cb43081
furia25 <val@ValPc.>
2025-02-15 03:50:15 +0100

    FINISHED

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:47:03 by val               #+#    #+#             */
-/*   Updated: 2025/02/14 15:18:53 by val              ###   ########.fr       */
+/*   Updated: 2025/02/15 03:44:36 by val              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	sort(t_data *data)
 	if (data->stack_a.top == 1)
 		if (data->stack_a.array[0] < data->stack_a.array[1])
 			return (swap_a(data), 1);
-	if (data->stack_a.top == 3 || data->stack_a.top == 4)
+	if (data->stack_a.top == 4)
 		return (sort_fourfive_a(data), 1);
 	else
 		return (sort_chunk_init(data), 1);
--- src/stacks_instructions/ins_clean.c
+++ src/stacks_instructions/ins_clean.c
@@ -6,7 +6,7 @@
 /*   By: val <val@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 20:28:16 by val               #+#    #+#             */
-/*   Updated: 2025/02/15 03:16:35 by val              ###   ########.fr       */
+/*   Updated: 2025/02/15 03:48:46 by val              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ static int	cstr(char *s1, char *s2)
 	return (ft_strcmp(s1, s2) == 0);
 }
 
-static void next_lsts(t_list **super_last, t_list **last, t_list **current)
+static void	next_lsts(t_list **super_last, t_list **last, t_list **current)
 {
 	if (!super_last)
 	{
 		*last = NULL;
 		*current = NULL;
+		return ;
 	}
 	*last = (*super_last)->next;
 	if (!*last)
+	{
 		*current = NULL;
+		return ;
+	}
 	*current = (*last)->next;
 }
 
@@ -36,33 +40,30 @@ static int	check_pair(t_list *last, t_list *current, char *a, char *b)
 		(cstr(last->content, b) && cstr(current->content, a)));
 }
 
-static void	clean_pair(t_list **super_last, t_list **last, t_list **current)
+static void	clean_pair(t_list **s_last, t_list **last, t_list **actual, char *s)
 {
-	char	*replace;
-
-	replace = NULL;
-	if (check_pair(*last, *current, SWAP_A, SWAP_B))
-		replace = SWAP_AB;
-	else if (check_pair(*last, *current, ROTATE_A, ROTATE_B))
-		replace = ROTATE_AB;
-	else if (check_pair(*last, *current, RROTATE_A, RROTATE_B))
-		replace = RROTATE_AB;
-	else if (check_pair(*last, *current, PUSH_A, PUSH_B) || \
-		check_pair(*last, *current, SWAP_A, SWAP_B) || \
-		check_pair(*last, *current, ROTATE_A, RROTATE_A) || \
-		check_pair(*last, *current, ROTATE_B, RROTATE_B))
-		replace = CLEAN_CONST;
-	if (replace && cstr(replace, CLEAN_CONST))
+	if (check_pair(*last, *actual, SWAP_A, SWAP_B))
+		s = SWAP_AB;
+	else if (check_pair(*last, *actual, ROTATE_A, ROTATE_B))
+		s = ROTATE_AB;
+	else if (check_pair(*last, *actual, RROTATE_A, RROTATE_B))
+		s = RROTATE_AB;
+	else if (check_pair(*last, *actual, PUSH_A, PUSH_B) || \
+		check_pair(*last, *actual, SWAP_A, SWAP_B) || \
+		check_pair(*last, *actual, ROTATE_A, RROTATE_A) || \
+		check_pair(*last, *actual, ROTATE_B, RROTATE_B))
+		s = CLEAN_CONST;
+	if (s && cstr(s, CLEAN_CONST))
 	{
-		(*super_last)->next = (*current)->next;
+		(*s_last)->next = (*actual)->next;
 		ft_lstdelone(*last, NULL);
-		ft_lstdelone(*current, NULL);
+		ft_lstdelone(*actual, NULL);
 	}
-	else if (replace)
+	else if (s)
 	{
-		(*last)->content = replace;
-		(*last)->next = (*current)->next;
-		ft_lstdelone(*current, NULL);
+		(*last)->content = s;
+		(*last)->next = (*actual)->next;
+		ft_lstdelone(*actual, NULL);
 	}
 }
 
@@ -78,7 +79,7 @@ void	instructions_clean(t_list **origin)
 	next_lsts(&super_last, &last, &current);
 	while (current)
 	{
-		clean_pair(&super_last, &last, &current);
+		clean_pair(&super_last, &last, &current, NULL);
 		super_last = super_last->next;
 		next_lsts(&super_last, &last, &current);
 	}
